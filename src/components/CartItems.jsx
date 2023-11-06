9d34839c3bee1a12a01a8153d6ed6b1f177cd487
Madhav Nyati <madhavnyati1606@gmail.com>
2023-11-06 21:08:09 +0530

    Final Changes

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -9,12 +9,12 @@ const CartItems = ({item}) => {
     return (
         <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500 ">
             <div className="w-full min-h-[150px] flex items-center gap-x-4">
-                <Link to={`/product/${id}`}>
+                <Link to={`/products/${id}`}>
                     <img className="max-w-[80px]" src={image} alt="" />
                 </Link>
                 <div  className='w-full flex flex-col'> 
                     <div className='flex justify-between mb-2'>
-                        <Link to={`/product/${id}`} className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline">{title}</Link>
+                        <Link to={`/products/${id}`} className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline">{title}</Link>
                         <div className='text-xl cursor-pointer'>
                             <IoMdClose onClick={()=> removeFromCart(id)} className='text-gray-500 hover:text-red-500 transition'/>
                         </div>
